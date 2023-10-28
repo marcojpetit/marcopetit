f2ac8b7fe340b6153cd63266ec2bb8d893ce05c5
Marco Petit <marcojpetit@gmail.com>
2023-10-28 13:30:31 -0300

    modelos creados

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=30)
+class Etiqueta(models.Model):
+    nombre = models.CharField(max_length=30)
+
+class Entrada(models.Model):
+    id_categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
+    titulo = models.CharField(max_length=150)
+    contenido = models.TextField()
+    id_autor = models.IntegerField()
+    fecha_publicacion = models.DateTimeField()
+    imagen_portada = models.ImageField()
+
+class Entrada_etiqueta(models.Model):
+    id_entrada = models.ForeignKey(Entrada, on_delete=models.CASCADE)
+    id_etiqueta = models.ForeignKey(Etiqueta, on_delete=models.PROTECT) 
