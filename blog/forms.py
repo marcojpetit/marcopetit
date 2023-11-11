f49760d7e7fa5cd9adcf56beb349c74d95dc3890
Marco Petit <marcojpetit@gmail.com>
2023-11-11 10:36:27 -0300

    frontend de la seleccion de categorias

--- blog/forms.py
+++ blog/forms.py
@@ -7,14 +7,14 @@ class CrearCategoriaFormulario(forms.Form):
     nombre = forms.CharField(label="Nombre de la categoría", max_length=30, widget=forms.TextInput(attrs={'class': "input__field cf-validate"}))
 
 class CrearEtiquetaFormulario(forms.Form):
-    nombre = forms.CharField(label="Nombre de la etiqueta", max_length=30, widget=forms.TextInput(attrs={'class': "input__field cf-validate"}))
+    nombre = forms.CharField(required=True, label="Nombre de la etiqueta", max_length=30, widget=forms.TextInput(attrs={'class': "input__field cf-validate"}))
     
 class BaseEntradaFormulario(forms.Form):
-    id_categoria = forms.ModelChoiceField(queryset=Categoria.objects.all(), label="Por favor seleccione una categoría", widget=forms.Select(attrs={'class': "input__field cf-validate"}))
+    id_categoria = forms.ModelChoiceField(queryset=Categoria.objects.all(), empty_label=None, label="Por favor seleccione una categoría", widget=forms.Select(attrs={'class': "input__field cf-validate"}))
     titulo = forms.CharField(label="Título de la entrada", max_length=150, widget=forms.TextInput(attrs={'class': "input__field cf-validate"}))
     contenido = RichTextFormField(label="Escribir el contenido aquí", widget=forms.Textarea(attrs={'class': "input__field cf-validate"}))
     imagen_portada = forms.ImageField(required=False, widget=forms.FileInput(attrs={'class': "input__field cf-validate"}))
-
+    
 class CrearEntradaFormulario(BaseEntradaFormulario):
     ...
 class ActualizarEntradaFormulario(BaseEntradaFormulario):
